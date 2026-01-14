f9bc05002c83b012f675c894c19447faaa5cf8b2
astroseer <ullasgowda.mac@gmail.com>
2026-01-14 22:46:11 +0530

    feat: day-1

---
+++ day1/learn.py
@@ -0,0 +1,17 @@
+# Single line print
+print("Hello!")
+
+# Multi line print
+print("Hello World! \nNow in second line \nand then comes third line")
+
+# String concat
+print("Hello" + " World")
+
+# User Input
+name = input("What is your name?")
+print("Hello " + name + "!")
+
+# Variable
+otherName = "Jack"
+print(len(otherName))
+
