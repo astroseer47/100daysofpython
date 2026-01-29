8cb100cf269358fbf0dc4b54e795c2d717a9ef0d
astroseer <ullasgowda.mac@gmail.com>
2026-01-29 16:47:19 +0530

    feat: day-24 - high-score

--- day21/score_board.py
+++ day21/score_board.py
@@ -3,9 +3,10 @@ from turtle import Turtle
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
+        self.highscore = 0
         self.score = 0
         self.penup()
-        self.goto(0,270)
+        self.goto(-150,270)
         self.color("white")
         self.update_score()
         self.hideturtle()
@@ -13,7 +14,7 @@ class ScoreBoard(Turtle):
 
     def update_score(self):
         self.clear()
-        self.write(f"Score: {self.score}",align="center", font=("Arial", 24, "normal"))
+        self.write(f"Score: {self.score}     High Score: {self.highscore}",align="center", font=("Arial", 24, "normal"))
 
     def increase_score(self):
         self.score += 1
@@ -25,3 +26,8 @@ class ScoreBoard(Turtle):
         self.goto(0,0)
         self.write(f"GAME OVER!",align="center", font=("Arial", 24, "normal"))
 
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.update_score()
